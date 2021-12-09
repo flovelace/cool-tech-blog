b02c3fb7ca94dcfaec6216a535e2bbec7c3a1fa8
fairfax <lovelace.af@gmail.com>
2021-12-08 23:57:08 -0500

    set up the user models and index

---
+++ models/index.js
@@ -0,0 +1,35 @@
+// import the models created to the index.js file
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+// Create the associations for the models
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NYLL'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
