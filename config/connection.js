8ce16988a5bac25b26dbe6b70a877c7529660bc4
fairfax <lovelace.af@gmail.com>
2021-12-12 13:46:30 -0500

    updated the connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
+// create connection to the db
+let sequelize;
 
-//create the connection to the database
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+// remote connection with jawsdb
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
     });
+}
 
 module.exports = sequelize;
