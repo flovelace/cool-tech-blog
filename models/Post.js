067f205b131d16c39a72b15a0156d34b620f6d0b
fairfax <lovelace.af@gmail.com>
2021-12-25 20:05:05 -0500

    working on layout for handlebars pages

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class Post extends Model { };
 
 // create fields/columns for Post model
 Post.init(
